9b1a5a0227c8455efd66d77b0b03371b4feab20c
John Pinkerton <jpinkerton88@gmail.com>
2015-02-25 15:50:35 -0500

    add update card method and refactor

--- lib/blossom_api/project/card.rb
+++ lib/blossom_api/project/card.rb
@@ -7,7 +7,7 @@ module BlossomApi
         if title_response.code == 200
           description_response = patch_card_description(title_response['id'], description)
           if description_response.code == 200
-            return true
+            return description_response['id']
           else
             return false
           end
@@ -16,30 +16,39 @@ module BlossomApi
         end
       end
 
+      def update_card(id, title, description)
+        response = patch_card(id, title, description)
+        if response.code == 200
+          return true
+        else
+          return false
+        end
+      end
+
       private
 
         def post_card_title(title)
-          HTTParty.post(create_card_url, body: create_card_options(title), headers: json_headers)
+          HTTParty.post(create_card_url, body: json_body(projectId: @project_id, title: title), headers: json_headers)
         end
 
         def patch_card_description(card_id, description)
-          HTTParty.patch(update_description_url(card_id), body: update_description_options(description), headers: json_headers)
+          HTTParty.patch(update_card_url(card_id), body: json_body(description: description), headers: json_headers)
+        end
+
+        def patch_card(card_id, title, description)
+          HTTParty.patch(update_card_url(card_id), body: json_body(title: title, description: description), headers: json_headers)
         end
 
         def create_card_url
           "#{BASE_URL}/cards/create_card?id=#{@organization_id}&accessToken=#{@access_token}"
         end
 
-        def update_description_url(card_id)
+        def update_card_url(card_id)
           "#{BASE_URL}/cards/#{card_id}?accessToken=#{@access_token}"
         end
 
-        def create_card_options(title)
-          { projectId: @project_id, title: title}.to_json
-        end
-
-        def update_description_options(description)
-          { description: description }.to_json
+        def json_body(*params)
+          params.first.to_json
         end
 
         def json_headers
